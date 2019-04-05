3ff1c5d0ffd5205efcdae5609064509c1434c7f9
Mario <erasuzmk@gmail.com>
2019-04-05 18:05:51 +0300

    adding css and js

--- js.js
+++ js.js
@@ -3,11 +3,27 @@ const addBtn = document.querySelector('.add-btn');
 const list = document.querySelector('.list');
 
 addBtn.addEventListener('click', function(e){
-    console.log(index.value);
 
-   const el =  document.createElement("li");
-   el.innerHTML += index.value;
+   if(index.value){
+    const el =  document.createElement("li");
+    const icon =  document.createElement("i");
+    const trash = document.createElement("i");
+    const span = document.createElement('span');
+
+    trash.classList.add('fas');
+    trash.classList.add('fa-trash-alt');
+    icon.classList.add('far');
+    icon.classList.add('fa-check-circle');
+   
+   span.appendChild(icon);
+   span.innerHTML +=index.value;
+   el.appendChild(span);
+   el.appendChild(trash);
     el.classList.add('list-el');
     list.appendChild(el);
+    
+    index.value = '';
+
+}
 
 });
